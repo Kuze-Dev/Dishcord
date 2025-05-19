d847e79db16bac5609e1bd00015227b3e8a07b49
mjtampus <michaeltampus24@gmail.com>
2025-05-19 15:38:44 +0800

    feature:login-with-sanctum

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Spatie\RouteAttributes\Attributes\Post;
+
+class AuthenticationController extends Controller
+{
+
+    #[
+        Post('api/register')
+    ]
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+            'address' => 'required',
+            'phone_number' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User registered successfully'
+        ]);
+        
+    }
+
+    #[
+        Post('api/login')
+    ]
+    public function login (Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found',
+            ]);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('API_TOKEN')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $token,
+            'user' => $user
+        ], 200);
+    }
+}
---
+++ database/migrations/2025_05_19_062611_create_user_information_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_information', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('bio')->nullable();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_information');
+    }
+};
